770c55f2a22e963235d9e390d3df3a7500bbc14f
Marika Hibi <skifkate.0621@gmail.com>
2024-04-29 01:53:31 +0000

    見た目を整え中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,7 +18,7 @@ class BooksController < ApplicationController
   	@book = Book.new(book_params)
   	@book.user_id = current_user.id
     if @book.save
-      flash[:notice] = "Book was successfully created."
+      flash[:notice] = "You have created book successfully."
       redirect_to book_path(@book.id)
     else
       @books = Book.all
@@ -29,14 +29,13 @@ class BooksController < ApplicationController
   def destroy
   	book = Book.find(params[:id])
   	 book.destroy
-    flash[:notice] = "Book was successfully destroyed."
   	redirect_to books_path
   end
 
   def update
     @book=Book.find(params[:id])
     if @book.update(book_params)
-    redirect_to book_path(@book),notice:"Book was successfully updated."
+    redirect_to book_path(@book),notice:"You have updated book successfully."
     else
     render:edit
     end
